128b250831d36bd2057c65519a83e263e2a494ec
William Doran <william.h.doran@gmail.com>
2016-01-07 06:13:47 -0500

    fixed problem with image upload and pit edit

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :myimage, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates :image, presence: true
+	validates :myimage, presence: true
 	validates :description, presence: true
 end
