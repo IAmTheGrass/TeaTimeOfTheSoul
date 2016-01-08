8abe059f51547e07c6f6aaf451d9e12a0cd6403a
William Doran <william.h.doran@gmail.com>
2016-01-08 10:50:20 -0500

    allow add customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+
 	def index
 		@customers = Customer.all
 	end
@@ -6,4 +7,14 @@ class CustomersController < ApplicationController
 	def show
 		@customer = Customer.find(params[:id])
 	end
+
+	def new
+    	@customer = Customer.new
+  	end
+
+  	def create
+  		@customer = Customer.new(params[:customer].permit(:name, :company_name, :customer_id))
+  		@customer.save
+  		redirect_to @customer
+  	end
 end
