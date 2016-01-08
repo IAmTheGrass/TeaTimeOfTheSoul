8b767c87b0488e85616b45ffe150c1a992eb4ef0
William Doran <william.h.doran@gmail.com>
2016-01-08 05:30:39 -0500

    added relational info

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,5 @@
 class Package < ActiveRecord::Base
+	has_one :product
+	has_many :sales
+	has_many :customers :through => :sales
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,6 @@
 class Sale < ActiveRecord::Base
+
+	has_one :customer
+	has_one :package
+	has_one :product, :through => :package
 end
