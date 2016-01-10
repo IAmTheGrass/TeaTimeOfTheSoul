68fae9f26ec88d5286dcb257f28fa51dba3f8859
William Doran <william.h.doran@gmail.com>
2016-01-10 17:13:34 -0500

    updated all but product show page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ActiveRecord::Base
 	has_many :sales
+	has_many :packages, through: :sales
+	has_many :products, through: :packages
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,4 +1,5 @@
 class Package < ActiveRecord::Base
 	belongs_to :product
 	has_many :sales
+	has_many :customers, through: :sales
 end
